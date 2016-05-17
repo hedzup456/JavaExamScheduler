ea423cae8bef2764b8e9f052438a75888ba95477
Richard Henry <richardhenry602@gmail.com>
2016-05-17 22:04:50 +0100

    Creation of exam object

---
+++ examTimer/Exam.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package examTimer;
+
+import java.util.Date;
+
+/**
+ * @author 		Richard Henry (richardhenry602@gmail.com)
+ * @since 		17 May 2016
+ *
+ */
+public class Exam {
+	private Date date;
+	private byte timeH, timeM;
+	private String location;
+	private String subject;
+}
